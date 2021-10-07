0488eb7b3b6e028dbb6639e38fc2bd3e83b0f39a
Meher <82945138+Meher450@users.noreply.github.com>
2021-10-07 17:33:44 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAYgNXKUj_bClnJERqWsYsuOe8HdrNuIJ4",
+  authDomain: "ultra-solution-328114.firebaseapp.com",
+  databaseURL: "https://ultra-solution-328114-default-rtdb.firebaseio.com",
+  projectId: "ultra-solution-328114",
+  storageBucket: "ultra-solution-328114.appspot.com",
+  messagingSenderId: "768906735423",
+  appId: "1:768906735423:web:11a6296e3fff2c74beed8a"
+  };
